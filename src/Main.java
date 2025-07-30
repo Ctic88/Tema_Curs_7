a8d028382c8ee551236855029d4d255e519bb151
Tic Cristian <cristi.tic88@gmail.com>
2025-07-30 21:53:19 +0300

    Tema Curs 7

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+// Tema introducere in Java
+
+public class Main {
+    public static void main(String[] args) {
+        // 1.
+        System.out.println("Hello");
+        System.out.println("Cristian");
+
+        // 2.
+        int numar1 = 10;
+        int numar2 = 33;
+        System.out.println(numar1 + numar2);
+        //sau
+        System.out.println(10 + 33);
+
+        // 3.
+        double num1 = 10;
+        double num2 = 3;
+        System.out.println(num1 / num2);
+        //sau
+        System.out.println((double) 10 / 3);
+
+        // 4.
+
+        // a
+        System.out.println(-5 + 8 * 6);
+
+        // b
+        System.out.println((55+9) % 9);
+
+        // c
+        System.out.println(20 + -3 * 5 / 8);
+
+        // d
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+}
